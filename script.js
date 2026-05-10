212b63099dc89b23590672309d57ae0f259c05d6
Thesayandas <142918700+Thesayandas@users.noreply.github.com>
2026-05-10 14:29:50 +0530

    Update

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function toggleMob() {
+  document.getElementById('mobMenu').classList.toggle('mob-open');
+}
+function submitForm() {
+  document.getElementById('modal').classList.add('show');
+}
+function closeModal() {
+  document.getElementById('modal').classList.remove('show');
+}
