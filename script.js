4834c5d22ac430daf71f2de74c943e5f3f272516
Thesayandas <142918700+Thesayandas@users.noreply.github.com>
2026-05-21 14:11:32 +0530

    Update

--- script.js
+++ script.js
@@ -9,3 +9,13 @@ function closeModal() {
   document.getElementById('modal').classList.remove('show');
   document.getElementById('modal').style.display = '';
 }
+
+function showPopup() {
+  document.getElementById('popupModal').classList.add('show');
+}
+
+function closePopup() {
+  document.getElementById('popupModal').classList.remove('show');
+}
+
+window.addEventListener('load', showPopup);
